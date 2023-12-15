0e01b47abcf64df1da9eedaba0d9b7f6ea84cdb5
Reynardthan Handoko <79642115+reynard26@users.noreply.github.com>
2023-12-16 05:08:12 +0700

    rollback user.js

--- backend/controller/Users.js
+++ backend/controller/Users.js
@@ -1,11 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const { Users } = require("../models/userModel");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { Users, sequelize } = require('../models/userModel'); 
-
-const router = express.Router();
-router.use(bodyParser.urlencoded({ extended: true }));
+const { cast } = require("sequelize");
 
 const getUsers = async (req, res) => {
     try {
@@ -58,56 +54,45 @@ const Register = async (req, res) => {
 
 const Login = async (req, res) => {
     try {
-        const { email, password } = req.body;
-
-        if (!email || !password) {
-            return res.status(400).json({ error: true, msg: "Email and password are required" });
-        }
-
-        const user = await Users.findOne({
+        const user = await Users.findAll({
             where: {
-                email: { [sequelize.Op.iLike]: email }
+                email: req.body.email
             }
         });
+        const match = await bcrypt.compare(req.body.password, user[0].password);
+        // if (!match) return res.status(400).json({ msg: "Password tidak sesuai" });
 
-        if (!user) {
-            return res.status(404).json({ error: true, msg: "Email not found" });
-        }
-
-        const match = await bcrypt.compare(password, user.password);
-
-        if (match) {
-            const userId = user.id;
-            const name = user.name;
-            const accessToken = jwt.sign({ userId, name, email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20s' });
-            const refreshToken = jwt.sign({ userId, name, email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
-
-            await Users.update({ refresh_token: refreshToken }, {
-                where: {
-                    id: userId
-                }
-            });
-
-            return res.status(200).json({
+        const userId = user[0].id;
+        const name = user[0].name;
+        const email = user[0].email;
+        const accessToken = jwt.sign({ userId, name, email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '20s' });
+        const refreshToken = jwt.sign({ userId, name, email }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '1d' });
+        await Users.update({ refresh_token: refreshToken }, {
+            where: {
+                id: userId
+            }
+        });
+        if (match) return res.status(200).json(
+            {
                 error: false,
-                msg: "Login successful",
+                msg: "Login Berhasil Dilakukan",
                 loginResult: {
                     userId,
                     name,
                     accessToken
                 }
             });
-        } else {
-            return res.status(400).json({
-                error: true,
-                msg: "Incorrect password"
-            });
+        else {
+            if (!match) return res.status(400).json(
+                { error: true },
+                { msg: "Password tidak sesuai" }
+            )
         }
+
     } catch (error) {
-        console.error("Login Error:", error);
-        res.status(500).json({ error: true, msg: "Internal Server Error: " + error.message });
+        res.status(404).json({ msg: "Email tidak ditemukan" });
     }
-};
+}
 
 const editUser = async (req, res) => {
     const { id, name } = req.body;
